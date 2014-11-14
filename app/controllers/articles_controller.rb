0fe14a29a70b455e27bea2f32a0b069e324ac0b7
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-14 14:45:33 +0000

    create: 記事投稿ページの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,20 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to blogs_path
+    else
+
+    end
+  end
+
+  private
+    def article_params
+      params[:article].permit(:title, :content)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :blog
   has_many :tag, through: :article_tags
+  validates :name, presence: true
 end
