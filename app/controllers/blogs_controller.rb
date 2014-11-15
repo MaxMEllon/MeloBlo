6761d792d694e957e926073c3c8f9192b59c5a9d
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-15 23:33:08 +0000

    dev: test用のブログcreateページ作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,26 @@
 class BlogsController < ApplicationController
 
   # before_action :authenticate_user!, only: [] # 閲覧制限
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render 'new'
+    end
+  end
 
   def index
     @blogs = Blog.all
     @articles = Article.all
   end
+
+  private
+    def blog_params
+      params[:blog].permit(:name)
+    end
 end
