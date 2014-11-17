1a2ef11307f1fb768611d2651af20c75388ca98e
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-17 10:54:55 +0000

    fix: sign-upの日本語化とcss適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,6 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  def after_sign_up_path_for(resource)
-    new_blog_path
-  end
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation, :admin)}
