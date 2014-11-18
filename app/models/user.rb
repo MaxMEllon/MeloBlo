9c611d6ad882ed86c14559e8b3d1e213643982c8
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-18 01:56:36 +0000

    fix: 日本語ファイルの不備修正，無駄なルーティングの指定削除，pramsに余計なカラムが入っていたのを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:name, :email]
   has_one :blog
@@ -14,5 +13,12 @@ class User < ActiveRecord::Base
     else
       where(conditions).first
     end
+  end
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
   end
 end
