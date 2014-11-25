322abf8ba6c9bfc925152f8a1e42085854abe798
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-26 04:49:41 +0900

    test: capybaraによるblog controllerのテスト追加

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -3,20 +3,26 @@ require 'rails_helper'
 RSpec.describe BlogsController, :type => :controller do
   describe "テスト対象：POST /blogs" do
 
-    before do
-      user = User.new
-      user.id = 1234
-      user.name = "test_user"
-      user.email = "test@test.com"
-      user.password = "testtest"
-      user.save
-      @user = User.where(name: :test_user).first
+    before "ユーザーのサインイン" do
+      @user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in "user_name",     with: @user.name
+      fill_in "user_email",    with: @user.email
+      fill_in "user_password", with: @user.password
+      click_button "ログイン"
+      visit root_path
+    end
+
+    context "ログインに成功しているかテスト" do
+      it do
+        should have_content @user.name
+      end
     end
 
     context "blogを作成するテスト" do
       it "create a blog" do
         visit new_user_blog_path(@user.id)
-        fill_in "title", with: "hoge"
+        fill_in "blog_title", with: "hoge"
         click_button "作成"
         page.should have_content("hoge")
       end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :user, :class => 'Users' do
+  factory :user, :class => 'User' do
     name "test"
     email "testtest@test.com"
     password "testtest"
